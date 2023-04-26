d4630da8692e54376e189c1715466fa5f7b59927
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-26 16:35:11 +0200

    elementi di style

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//prendiamo dall'html la griglia.
+let grid = document.getElementById("griglia");
+
+// al click del bottone, dobbiamo generare la gliglia con i 100 quadratini.
+let btn = document.getElementById("bottone");
+
+btn.addEventListener("click", function(){
+
+    let nuovoDiv = document.createElement("div");
+    console.log("hai crato un nuovo div");
+
+});
