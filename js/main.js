279d96489ffe8c3f85e6f093b4f65adf25aa71aa
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-26 18:28:17 +0200

    console log scelta numero

--- js/main.js
+++ js/main.js
@@ -19,6 +19,11 @@ btn.addEventListener("click", function(){
         // inserire questa variabile all'interno del dom con la clesse per creare il div
         document.getElementById("griglia").appendChild(nuovoDiv).className = "quadratino";
         nuovoDiv.innerHTML = nuovoDiv.innerHTML + i;
+
+        nuovoDiv.addEventListener("click", function(){
+            this.classList.toggle("cambioColore");
+            console.log("hei scelto il n:", i);
+        })
     }
     
 })
