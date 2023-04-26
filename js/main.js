aee1476fd2b339cd43211282248b098ef01b9960
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-26 17:20:14 +0200

    ciclo for

--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@ let grid = document.getElementById("griglia");
 // al click del bottone, dobbiamo generare la gliglia con i 100 quadratini.
 let btn = document.getElementById("bottone");
 
+
+
+
+//al ckcik del bottone creo un nuovo div (quadratino)
 btn.addEventListener("click", function(){
+    
+    //ciclo per creare 100 div
+    for (let i = 0; i < 100; i++) {
+        
+        // creazione div appeso alla variabile 
+        let nuovoDiv = document.createElement("div");
 
-    let nuovoDiv = document.createElement("div");
-    console.log("hai crato un nuovo div");
+        // inserire questa variabile all'interno del dom con la clesse
+        document.getElementById("griglia").appendChild(nuovoDiv).className = "quadratino";
+    }
+    
+})
 
-});
+
